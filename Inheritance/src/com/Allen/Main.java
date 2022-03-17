5eba20f2d0a58a8281a38ffda10865c5f60a5539
Allen Harper <aharper1609@gmail.com>
2022-03-16 22:01:07 -0500

    Inheritence

--- Inheritance/src/com/Allen/Main.java
+++ Inheritance/src/com/Allen/Main.java
@@ -3,7 +3,17 @@ package com.Allen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// inheritance  == the process where one class acquires the attributes and methods of another.
+        Car car = new Car();
+
+        car.go();
+
+        Bicycle bike = new Bicycle();
+
+        bike.stop();
+
+        System.out.println(car.speed);
+        System.out.println(bike.speed);
 
     }
 }
