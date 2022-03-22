bc6dde9dabf435b3468b66e52c2e4cfb8b17611c
Allen Harper <aharper1609@gmail.com>
2022-03-22 16:21:15 -0500

    enums

--- Enums/src/com/Allen/Main.java
+++ Enums/src/com/Allen/Main.java
@@ -1,8 +1,32 @@
 package com.Allen;
 
+
 public class Main {
 
+    // enum = enumerated (ordered listing of items in a collection)
+    // grouping of constants that behave similarly to objects
+
     public static void main(String[] args) {
-	// write your code here
+
+        Planet myPlanet = Planet.PLUTO;
+
+        canILiveHere(myPlanet);
+
     }
+
+    static void canILiveHere(Planet myPlanet){
+
+        switch(myPlanet){
+            case EARTH:
+                System.out.println("You can live here :)");
+                System.out.println("This is planet #" + myPlanet.number);
+                break;
+            default:
+                System.out.println("You can't live here....yet");
+                System.out.println("This is planet #" + myPlanet.number);
+                break;
+        }
+    }
+
+
 }
