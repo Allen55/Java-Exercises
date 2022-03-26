801b2499a52d6c0eade2278878b9003cf0b694bb
Allen Harper <aharper1609@gmail.com>
2022-03-25 21:30:06 -0500

    BinarySearch

---
+++ DataStructuresAlgorithms/BinarySearch/src/com/Allen/Main.java
@@ -0,0 +1,37 @@
+package com.Allen;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[1000000];
+        int target = 77777;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        int index = binarySearch(array, target);
+    }
+
+    private static int binarySearch(int[] array, int target){
+
+        int low = 0;
+        int high = array.length - 1;
+        int count = 0; // optional
+
+        while (low <= high){
+            int middle = low + (high-low) / 2;
+            int value = array[middle];
+            count++;
+            System.out.println("Step: " + count + " middle: " + value);
+
+            if (value < target) low = middle + 1;
+            else if (value > target) high = middle -1;
+            else return middle; // target is found
+
+
+        }
+
+        return -1; // sentinal value, target not found
+    }
+}
