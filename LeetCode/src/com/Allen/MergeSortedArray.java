60f0f53e22d677a94e02e1f74faadbb5411f6ac8
Allen Harper <aharper1609@gmail.com>
2022-04-04 20:47:39 -0500

    Merge Sorted Array

--- LeetCode/src/com/Allen/MergeSortedArray.java
+++ LeetCode/src/com/Allen/MergeSortedArray.java
@@ -21,10 +21,21 @@ The result of the merge is [1].
 
 public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        // instantiate a new array with length based on m + n
-      // iterate through both arrays
-      // compare
-        int[] arr = new int[m+n];
+        m--; n--;
+
+        int index = nums1.length-1;
+        while(index >= 0){
+            if(m < 0){
+                nums1[index] = nums2[n--];
+            } else if(n < 0){
+                nums1[index] = nums1[m--];
+            } else {
+                if(nums1[m] > nums2[n]){
+                    nums1[index] = nums2[n--];
+                }
+            }
+            index--;
+        }
 
 
     }
